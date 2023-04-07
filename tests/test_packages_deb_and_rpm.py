7b4888e5a628f34cc0a5ecda6d30438e4374496d
acolyte-py <spaik.mironov@yandex.ru>
2023-04-07 20:32:26 +0700

    init-2

--- tests/test_packages_deb_and_rpm.py
+++ tests/test_packages_deb_and_rpm.py
@@ -7,12 +7,12 @@ from subprocess import check_call
 def test_install_deb_package():
     if not which('dpkg'):
         pytest.skip('dpkg not found')
-    check_call(['sudo', 'dpkg', '-i', '/path/to/*.deb'])
+    check_call(['sudo', 'dpkg', '-i', '/home/tester/*.deb'])
     assert which('*.deb') is not None
 
 
 def test_install_rpm_package():
     if not which('rpm'):
         pytest.skip('rpm not found')
-    check_call(['sudo', 'rpm', '-i', '/home/test/*.rpm'])
+    check_call(['sudo', 'rpm', '-i', '/home/tester/*.rpm'])
     assert which('*.rpm') is not None
